7454daea485e967ec5b2519360a3755a21b2e633
tanyap <biddanda.tanya@gmail.com>
2020-06-15 15:18:59 -0700

    adds working employee.js with passing test

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// function promptUser(){
+//     return inquirer.prompt([
+//         {
+//             type: "input",
+//             name: "name",
+//             message: "Please enter your full name: "
+//         }
+//     ])
+// }
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
